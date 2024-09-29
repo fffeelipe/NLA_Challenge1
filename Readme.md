ff73c535f72ad921ed7269873110a2c43dcf74b7
fffeelipe <lfepiar@unal.edu.co>
2024-09-29 13:46:36 +0200

    first 7 tasks done

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Tasks
+
+1. Load the image as an Eigen matrix with size m × n. Each entry in the matrix corresponds to a pixel on the screen and takes a value somewhere between 0 (black) and 255 (white). Report the size of the matrix.
+2. Introduce a noise signal into the loaded image by adding random fluctuations of color ranging between [−50, 50] to each pixel. Export the resulting image in .png and upload it.
+3. Reshape the original and noisy images as vectors v and w, respectively. Verify that each vector has m n components. Report here the Euclidean norm of v.
+4. Write the convolution operation corresponding to the smoothing kernel Hav2 as a matrix vector multiplication between a matrix A1 having size mn × mn and the image vector. Report the number of non-zero entries in A1.
+5. Apply the previous smoothing filter to the noisy image by performing the matrix vector multiplication A1w. Export and upload the resulting image.
+6. Write the convolution operation corresponding to the sharpening kernel Hsh2 as a matrix vector multiplication by a matrix A2 having size mn × mn. Report the number of non-zero entries in A2. Is A2 symmetric?
+7. Apply the previous sharpening filter to the original image by performing the matrix vector multiplication A2v. Export and upload the resulting image.
+8. Export the Eigen matrix A2 and vector w in the .mtx format. Using a suitable iterative solver and preconditioner technique available in the LIS library compute the approximate solution to the linear system A2x = w prescribing a tolerance of 10−9. Report here the iteration count and the final residual.
+9. Import the previous approximate solution vector x in Eigen and then convert it into a .png image. Upload the resulting file here.
+10.  Write the convolution operation corresponding to the detection kernel Hlap as a matrix vector multiplication by a matrix A3 having size mn × mn. Is matrix A3 symmetric?
+11. Apply the previous edge detection filter to the original image by performing the matrix vector multiplication A3 v. Export and upload the resulting image.
+12. Using a suitable iterative solver available in the Eigen library compute the approximate solution of the linear system (I+A3)y = w, where I denotes the identity matrix, prescribing a tolerance of 10−10. Report here the iteration count and the final residual.
+13. Convert the image stored in the vector y into a .png image and upload it.
+14. Comment the obtained results.
+
--- main.cpp
+++ main.cpp
@@ -1,77 +1,132 @@
-#include <Eigen/Dense>
+#include <Eigen/Sparse>
 #include <iostream>
 
 #define STB_IMAGE_IMPLEMENTATION
+
 #include "stb_image.h"
+
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "stb_image_write.h"
 
 using namespace Eigen;
 
 const std::string IMAGE_NAME = "Albert_Einstein_Head.jpg";
+const double EPS = 0.00001;
 
-
-void save_img(MatrixXd m, int width, int height, std::string outputName){
+void save_img(MatrixXd m, int width, int height, std::string outputName) {
     Matrix<unsigned char, Dynamic, Dynamic, RowMajor> img(height, width);
-            img = m.unaryExpr([](double val) -> unsigned char {
-                return static_cast<unsigned char>(val);
-            });
+    img = m.unaryExpr([](double val) -> unsigned char { return static_cast<unsigned char>(val); });
 
-            // Save the image using stb_image_write
-            if (stbi_write_png(outputName.c_str(), width, height, 1,
-                               img.data(), width) == 0) {
-                std::cerr << "Error: Could not save " + outputName + " image" << std::endl;
+    // Save the image using stb_image_write
+    if (stbi_write_png(outputName.c_str(), width, height, 1,
+                       img.data(), width) == 0) {
+        std::cerr << "Error: Could not save " + outputName + " image" << std::endl;
 
-                return ;
+        return;
+    }
+
+    std::cout << "image saved to " << outputName << std::endl;
+}
+
+// assume convolution matrix to be 3x3
+std::tuple<SparseMatrix<double>, VectorXd>
+applyConvolution(VectorXd img, MatrixXd convolutionMatrix, int width, int height) {
+    SparseMatrix<double> filterMatrix(img.size(), img.size());
+    std::vector<Triplet<double> > filter_data;
+    for (int idx = 0; idx < img.size(); idx++) {
+        int x = idx / width;
+        int y = idx % width;
+
+        for (int i = -1; i < 2; i++) {
+            for (int j = -1; j < 2; j++) {
+                if (y + i < 0 || y + i >= width || x + j < 0 || x + j >= height) continue;
+
+                filter_data.emplace_back(idx, (y + i) + (j + x) * width, convolutionMatrix(i + 1, j + 1));
             }
 
-            std::cout << "image saved to " << outputName << std::endl;
+        }
+
+    }
+
+    filterMatrix.setFromTriplets(filter_data.begin(), filter_data.end());
+
+    return {filterMatrix, filterMatrix * img};
 
 }
 
-int main(int argc, char* argv[]) {
-            srand(777); //repetible randoms
+int main(int argc, char *argv[]) {
+    srand(777); // repetible randoms
+    MatrixXd H_av2{{1, 1, 1},
+                   {1, 1, 1},
+                   {1, 1, 1}};
+    H_av2 *= 1.0 / 9;
 
-            // Load the image using stb_image
-            int width, height, channels;
-                unsigned char* image_data = stbi_load(IMAGE_NAME.c_str(), &width, &height, &channels, 1);  // Force 1 channel
+    MatrixXd H_sh2{{0,  -3, 0},
+                   {-1, 9,  -3},
+                   {0,  -1, 0}};
 
-            if (!image_data) {
-                std::cerr << "Error: Could not load image " << IMAGE_NAME << std::endl;
-                return 1;
-            }
+    // Load the image using stb_image
+    int width, height, channels;
+    unsigned char *image_data = stbi_load(IMAGE_NAME.c_str(), &width, &height, &channels, 1); // Force 1 channel
 
-            std::cout << "Image loaded: " << width << "x" << height << " with " << channels << " channels." << std::endl;
+    if (!image_data) {
+        std::cerr << "Error: Could not load image " << IMAGE_NAME << std::endl;
+        return 1;
+    }
 
-            MatrixXd gray(height, width);
+    // task 1
+    std::cout << "Image loaded: " << width << "x" << height << " with " << channels << " channels." << std::endl;
 
-            // Fill the matrix with image data    
-            for (int i = 0; i < height; ++i) {
-                for (int j = 0; j < width; ++j) {
-                    gray(i, j) = static_cast<double>(image_data[(i * width + j)]);
-                }
-            }
-            // Free memory!!!
-            stbi_image_free(image_data);
+    MatrixXd gray(height, width);
 
-            save_img(gray, width, height, "initial.png");
-            
-            MatrixXd noisy = gray.unaryExpr([](double val) -> double {
-                return std::min(255.0, std::max(0.0, static_cast<double>(val + rand() % 101 - 50)));
-            });
-
-            save_img(noisy, width, height, "noisy.png");
-
-            auto gray_vector = gray.reshaped();
-            auto noisy_vector = noisy.reshaped();
-
-            double gray_vector_norm = std::sqrt(gray_vector.dot(gray_vector));
-            double noisy_vector_norm = std::sqrt(noisy_vector.dot(noisy_vector));
-
-            std::cout<<"norm of original vector is: "<<gray_vector_norm<<"\n";
-            std::cout<<"norm of noisy vector is: "<<noisy_vector_norm<<"\n";
-
-
-
-        return 0;           
+    // Fill the matrix with image data
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            gray(i, j) = static_cast<double>(image_data[(i * width + j)]);
         }
+    }
+    // Free memory!!!
+    stbi_image_free(image_data);
+
+    MatrixXd noisy = gray.unaryExpr([](double val) -> double {
+        return std::min(255.0, std::max(0.0, static_cast<double>(val + rand() % 101 - 50)));
+    });
+
+    // task 2
+    save_img(noisy, width, height, "noisy.png");
+
+    auto gray_vector = gray.transpose().reshaped();
+    auto noisy_vector = noisy.transpose().reshaped();
+
+    std::cout << (gray_vector.dot(gray_vector)) << std::endl;
+    double gray_vector_norm = std::sqrt(gray_vector.dot(gray_vector));
+    double noisy_vector_norm = std::sqrt(noisy_vector.dot(noisy_vector));
+
+
+    // task 3
+    std::cout << "size of original vector is: " << gray_vector.size() << "\n";
+    std::cout << "size of noisy vector is: " << noisy_vector.size() << "\n";
+
+    std::cout << "norm of original vector is: " << gray_vector_norm << "\n";
+    std::cout << "norm of noisy vector is: " << noisy_vector_norm << "\n";
+
+    // task 4
+    auto [A1, smooth_noisy_vector] = applyConvolution(noisy_vector, H_av2, width, height);
+    printf("number of non-zero entries in matrix A1: %i\n", A1.nonZeros());
+    // task 5
+    save_img(smooth_noisy_vector, width, height, "smooth_noisy_vector.png");
+
+
+    // task 6
+    auto [A2, sharpened_original] = applyConvolution(gray_vector, H_sh2, width, height);
+    printf("number of non-zero entries in Matrix A2: %i, is symetrical? %s\n", A2.nonZeros(),
+           A2.isApprox(A2.transpose()) ? "true" : "false");
+
+    // task 7
+    save_img(sharpened_original, width, height, "sharpened_original.png");
+    return 0;
+
+    //task 8
+
+}
